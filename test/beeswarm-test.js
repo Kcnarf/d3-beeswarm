3c4cf3ed0426f95726e5c45ede29eae19f9ef37d
Kcnarf <fl.franck.lebeau+github@gmail.com>
2016-10-27 15:04:54 +0200

    use 'make a D3 plugin' folder structure

---
+++ test/beeswarm-test.js
@@ -0,0 +1,7 @@
+var tape = require("tape"),
+    beeswarm = require("../");
+
+tape("beeswarm() returns the answer to the ultimate question of life, the universe, and everything.", function(test) {
+  test.equal(42, 42);
+  test.end();
+});
