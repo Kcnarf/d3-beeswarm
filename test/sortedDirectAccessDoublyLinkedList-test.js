79830cca8d14d63b7a4e00ba64cb2f9bd7178362
Kcnarf <fl.franck.lebeau+github@gmail.com>
2016-11-07 09:25:21 +0100

    add test for SDADLL.addMany()

--- test/sortedDirectAccessDoublyLinkedList-test.js
+++ test/sortedDirectAccessDoublyLinkedList-test.js
@@ -89,11 +89,31 @@ tape("SDADLL.add() should maintain sort", function(test) {
           sdadll.dln(datum2).next === sdadll.dln(datum3) &&
           sdadll.dln(datum3).prev === sdadll.dln(datum2));
   test.end();
+});
 
-  tape("SDADLL.add() should allow chaining", function(test) {
-    var sdadll = new SDADLL(),
-        datum0 = {id: 0, value: -1};
-    test.ok(sdadll.add(datum0) === sdadll);
-    test.end();
-  });
+tape("SDADLL.add() should allow chaining", function(test) {
+  var sdadll = new SDADLL(),
+      datum0 = {id: 0, value: -1};
+  test.ok(sdadll.add(datum0) === sdadll);
+  test.end();
+});
+
+tape("SDADLL.addMany() should add() each datum", function(test) {
+  var sdadll = new SDADLL(),
+      data0 = [{id: 0, value: 0}, {id: 1, value: 1}],
+      data1 = [{id: 2, value: 2}],
+      data2 = [];
+  sdadll.addMany(data0);
+  test.ok(sdadll.size === 2);
+  sdadll.addMany(data1);
+  test.ok(sdadll.size === 3);
+  sdadll.addMany(data2);
+  test.ok(sdadll.size === 3);
+  test.end();
+});
+
+tape("SDADLL.addMany() should allow chaining", function(test) {
+  var sdadll = new SDADLL();
+  test.ok(sdadll.addMany([]) === sdadll);
+  test.end();
 });
